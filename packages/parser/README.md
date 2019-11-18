38c856c6d7850163a1b80337c157b371e4d4bd74
Njibhu <manu@njibhu.eu>
2019-11-18 01:42:12 +0100

    Implement seek and constructor

--- packages/parser/README.md
+++ packages/parser/README.md
@@ -1,1 +1,4 @@
 # t3d-parser
+
+Implementation is based on the work done by kig on [Datastream.js](github.com/kig/Datastream.js).
+It is tailored to the use for the t3d library.
--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,34 +1,41 @@
-import * as Types from "./types";
-import { DataType } from "./internals";
+import * as Types from './types';
+import {DataType} from './internals';
 
 export enum Endianness {
   LITTLE_ENDIAN,
-  BIG_ENDIAN
+  BIG_ENDIAN,
 }
 
 export default class DataReader {
   private arrayBuffer: ArrayBuffer;
+  private byteLength: number;
+  private dataView: DataView;
   readonly endianness: Endianness;
   private position = 0;
 
   constructor(
     arrayBuffer: ArrayBuffer,
-    endianness: Endianness = Endianness.LITTLE_ENDIAN
+    endianness: Endianness = Endianness.LITTLE_ENDIAN,
   ) {
     this.arrayBuffer = arrayBuffer;
     this.endianness = endianness;
+    this.byteLength = this.arrayBuffer.byteLength;
+    this.dataView = new DataView(this.arrayBuffer);
   }
 
   /**
    * Move the cursor position
    */
-  seek(position: number): void {}
+  seek(position: number): void {
+    const npos = Math.max(0, Math.min(this.byteLength, pos));
+    this.position = isNaN(npos) || !isFinite(npos) ? 0 : npos;
+  }
 
   /**
    * Returns true if the cursor is at the end of the buffer false otherwise
    */
   isEof(): boolean {
-    return false;
+    return this.position >= this.byteLength;
   }
 
   readType(type: DataType): any {}
