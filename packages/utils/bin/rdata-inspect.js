a0c5d3c460754c3cd76caba587235206e8aa8d20
Njibhu <manu@njibhu.eu>
2019-11-24 21:24:43 +0100

    Do parsing inside the script

--- packages/utils/bin/rdata-inspect.js
+++ packages/utils/bin/rdata-inspect.js
@@ -1,19 +1,60 @@
 #!/usr/bin/env node
 
-const r2pipe = require('r2pipe');
+const r2pipe = require("r2pipe");
+const { StringDecoder } = require("string_decoder");
+const ascii = new StringDecoder("ascii");
 
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error('Require path to executable file');
+  console.error("Require path to executable file");
   process.exit(1);
 }
 
-console.log('Opening exe file...');
+console.log("Opening exe file...");
 const r2 = r2pipe.open(filePath);
-const segments = r2.cmdj('iSj');
+const segments = r2.cmdj("iSj");
 
-const rdata = segments.find(i => i.name === '.rdata');
+const rdata = segments.find(i => i.name === ".rdata");
+const stringFound = [];
+
+if (rdata) {
+  const { vsize, vaddr } = rdata;
+  console.log("Found rdata!");
+  r2.cmd(`s ${vaddr}`);
+
+  const arrayBuffer = new Uint8Array(r2.cmdj(`pxj ${vsize}`));
+  const dataView = new DataView(arrayBuffer.buffer);
+
+  for (let cursor = 0; cursor < vsize; cursor += 4) {
+    const buffer = arrayBuffer.slice(cursor, cursor + 4);
+
+    if (isFourAscii(buffer)) {
+      const structPtr = getUint64(dataView, cursor + 8, true);
+      if (structPtr > vaddr && structPtr < vaddr + vsize) {
+        // Register found string
+        stringFound.push(ascii.write(arrayBuffer.slice(cursor, cursor + 4)));
+      }
+    }
+  }
+
+  console.log(stringFound);
+}
+
+r2.quit();
+
+//
+// Functions
+//
+
+function isFourAscii(buffer) {
+  return (
+    isAscii(buffer[0]) &&
+    isAscii(buffer[1]) &&
+    isAscii(buffer[2]) &&
+    (buffer[3] == 0 || isAscii(buffer[3]))
+  );
+}
 
 function isAscii(byte) {
   return (
@@ -23,52 +64,16 @@ function isAscii(byte) {
   );
 }
 
-const stringFound = [];
+// This is coming from MDN
+function getUint64(dataview, byteOffset, littleEndian) {
+  // split 64-bit number into two 32-bit (4-byte) parts
+  const left = dataview.getUint32(byteOffset, littleEndian);
+  const right = dataview.getUint32(byteOffset + 4, littleEndian);
 
-if (rdata) {
-  console.log('Found rdata!');
-  console.log(rdata);
-  const maxRDataAddress = rdata.vaddr + rdata.vsize;
+  // combine the two 32-bit values
+  const combined = littleEndian
+    ? left + 2 ** 32 * right
+    : 2 ** 32 * left + right;
 
-  let cursor = rdata.vaddr;
-  r2.cmd(`s ${cursor}`);
-  let progress = 0;
-  console.log(progress);
-  const fullBuffer = r2.cmdj(`pxj ${rdata.vsize}`);
-
-  while (cursor < maxRDataAddress) {
-    const buffer = fullBuffer.slice(
-      cursor - rdata.vaddr,
-      cursor - rdata.vaddr + 4,
-    );
-
-    if (
-      isAscii(buffer[0]) &&
-      isAscii(buffer[1]) &&
-      isAscii(buffer[2]) &&
-      (buffer[3] == 0 || isAscii(buffer[3]))
-    ) {
-      r2.cmd(`s ${cursor + 8}`);
-      const structPtr = r2.cmdj('pfjN8')[0].value;
-      if (structPtr > rdata.vaddr && structPtr < rdata.vaddr + rdata.vsize) {
-        r2.cmd(`s -8`);
-        stringFound.push(r2.cmd('ps'));
-        console.log(r2.cmd('ps'));
-      }
-    }
-
-    const newProgress = Math.floor(
-      ((cursor - rdata.vaddr) / rdata.vsize) * 100,
-    );
-    if (progress != newProgress) {
-      progress = newProgress;
-      console.log(progress);
-    }
-
-    cursor += 4;
-  }
-
-  console.log(stringFound);
+  return combined;
 }
-
-r2.quit();
