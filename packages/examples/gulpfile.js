f84efba45d8e5a04c15a0aa8fec04411c002b38f
Njibhu <manu@njibhu.eu>
2020-05-03 01:23:27 +0200

    Add watch

--- packages/examples/gulpfile.js
+++ packages/examples/gulpfile.js
@@ -2,57 +2,98 @@
 const gulp = require("gulp");
 const browserify = require("browserify");
 const sourcemaps = require("gulp-sourcemaps");
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const rename = require('gulp-rename');
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const rename = require("gulp-rename");
 
-function copyExampleAssets(asset){
-  gulp.src(`./src/${asset}`)
-    .pipe(rename(asset))
-    .pipe(gulp.dest('./dist/'));
+function copyExampleAssets(asset) {
+  gulp.src(`./src/${asset}`).pipe(rename(asset)).pipe(gulp.dest("./dist/"));
 }
 
-function copyStaticAssets(){
+function copyStaticAssets() {
   return [
-    gulp.src(`./node_modules/t3d-toolsjs/t3dworker.js`)
+    gulp
+      .src(`./node_modules/t3d-toolsjs/t3dworker.js`)
       .pipe(rename("t3dworker.js"))
-      .pipe(gulp.dest('./dist/static')),
-    gulp.src([`./node_modules/w2ui/w2ui-1.4.3.min.js`, `./node_modules/w2ui/w2ui-1.4.3.min.css`])
-      .pipe(gulp.dest('./dist/static'))
-  ]
+      .pipe(gulp.dest("./dist/static")),
+    gulp
+      .src([
+        `./node_modules/w2ui/w2ui-1.4.3.min.js`,
+        `./node_modules/w2ui/w2ui-1.4.3.min.css`,
+      ])
+      .pipe(gulp.dest("./dist/static")),
+  ];
 }
 
 function buildExample(entryPoint) {
   const b = browserify({
     entries: [`./src/${entryPoint}`],
-    debug: true
+    debug: true,
   });
 
-  return b.bundle()
+  return b
+    .bundle()
     .pipe(source(`./src/${entryPoint}`))
     .pipe(rename(entryPoint))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write("./"))
-
-    .pipe(gulp.dest("./dist"))
+    .pipe(gulp.dest("./dist"));
 }
 
-gulp.task("examples", () =>
-  Promise.all([
-    copyExampleAssets("index.html"),
+function statics() {
+  return [copyExampleAssets("index.html"), ...copyStaticAssets()];
+}
+
+function MapRenderer() {
+  return [
     buildExample("MapRenderer/index.js"),
     copyExampleAssets("MapRenderer/index.html"),
+  ];
+}
+
+function LocalReaderV2() {
+  return [
     buildExample("LocalReaderV2/index.js"),
     copyExampleAssets("LocalReaderV2/index.html"),
+  ];
+}
+
+function ModelRenderer() {
+  return [
     buildExample("ModelRenderer/index.js"),
     copyExampleAssets("ModelRenderer/index.html"),
+  ];
+}
+
+function Tyria2D() {
+  return [
     buildExample("Tyria2D/index.js"),
     copyExampleAssets("Tyria2D/index.html"),
-    ...copyStaticAssets()
+  ];
+}
+
+gulp.task("default", () =>
+  Promise.all([
+    ...statics(),
+    ...MapRenderer(),
+    ...LocalReaderV2(),
+    ...ModelRenderer(),
+    ...Tyria2D(),
   ])
 );
 
+gulp.task("watch", () => {
+  const delay = 500;
 
-
-gulp.task("default", gulp.series(["examples"]));
+  gulp.watch("src/MapRenderer/*", { delay }, Promise.all(MapRenderer()));
+  gulp.watch("src/LocalReaderV2/*", { delay }, Promise.all(LocalReaderV2()));
+  gulp.watch("src/ModelRenderer/*", { delay }, Promise.all(ModelRenderer()));
+  gulp.watch("src/Tyria2D/*", { delay }, Promise.all(Tyria2D()));
+  gulp.watch("src/index.html", { delay }, Promise.all(statics()));
+  gulp.watch(
+    "node_modules/t3d-lib/src/**/*",
+    { delay },
+    gulp.series("default")
+  );
+});
