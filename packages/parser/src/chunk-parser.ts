34090bd0de43b4d6890261c4fcfba1dbb623b56b
Njibhu <manu@njibhu.eu>
2020-08-14 16:10:00 +0200

    Continue implementation of the chunk parser

--- packages/parser/src/chunk-parser.ts
+++ packages/parser/src/chunk-parser.ts
@@ -43,104 +43,7 @@ class ChunkParser implements Definition {
     };
   }
 
-  Float32(dv: DataView, pos: number): ParseFunctionReturn {
-    return { newPosition: pos + 4, data: dv.getFloat32(pos, true) };
-  }
-
-  Float64(dv: DataView, pos: number): ParseFunctionReturn {
-    return { newPosition: pos + 8, data: dv.getFloat64(pos, true) };
-  }
-
-  Uint8(dv: DataView, pos: number): ParseFunctionReturn {
-    return { newPosition: pos + 1, data: dv.getUint8(pos) };
-  }
-
-  Uint16(dv: DataView, pos: number): ParseFunctionReturn {
-    return { newPosition: pos + 2, data: dv.getUint16(pos, true) };
-  }
-
-  Uint32(dv: DataView, pos: number): ParseFunctionReturn {
-    return { newPosition: pos + 4, data: dv.getUint32(pos, true) };
-  }
-
-  Uint64(dv: DataView, pos: number): ParseFunctionReturn {
-    return {
-      newPosition: pos + 4,
-      data: dv.getUint32(pos, true) + 2 ** 32 * dv.getUint32(pos + 8, true),
-    };
-  }
-
-  CString(dv: DataView, pos: number): ParseFunctionReturn {
-    const u8 = new Uint8Array(dv.buffer, pos);
-    const end = u8.findIndex((v) => v === 0);
-
-    return {
-      newPosition: pos + end,
-      data: String.fromCharCode.apply(null, new Uint8Array(u8.slice(0, end))),
-    };
-  }
-
-  FixedArray(dv: DataView, pos: number, type: DataType | string, length: number): ParseFunctionReturn {
-    return {
-      // TODO
-      newPosition: -1,
-      data: {},
-    };
-  }
-
-  DynArray(dv: DataView, pos: number, type: DataType | string): ParseFunctionReturn {
-    return {
-      // TODO
-      newPosition: -1,
-      data: {},
-    };
-  }
-
-  RefArray(dv: DataView, pos: number, type: DataType | string): ParseFunctionReturn {
-    return {
-      // TODO
-      newPosition: -1,
-      data: {},
-    };
-  }
-
-  Pointer(dv: DataView, pos: number, type: DataType | string): ParseFunctionReturn {
-    return {
-      // TODO
-      newPosition: -1,
-      data: {},
-    };
-  }
-
-  String16(dv: DataView, pos: number): ParseFunctionReturn {
-    return {
-      // TODO
-      newPosition: -1,
-      data: {},
-    };
-  }
-
-  Filename(dv: DataView, pos: number): ParseFunctionReturn {
-    return {
-      // TODO
-      newPosition: -1,
-      data: {},
-    };
-  }
-
-  Fileref(dv: DataView, pos: number): ParseFunctionReturn {
-    return {
-      // TODO
-      newPosition: -1,
-      data: {},
-    };
-  }
-
-  Unknown(dv: DataView, pos: number): ParseFunctionReturn {
-    throw new Error("Could not parse unknown data");
-  }
-
-  parseType(dv: DataView, pos: number, typeDefinitionName: string): ParseFunctionReturn {
+  public parseType(dv: DataView, pos: number, typeDefinitionName: string): ParseFunctionReturn {
     const parsedObject = {};
     let position = pos;
     const definition = this.definitions[typeDefinitionName];
@@ -154,5 +57,133 @@ class ChunkParser implements Definition {
       newPosition: position,
       data: parsedObject,
     };
+  }
+
+  /**
+   *      Parsers
+   **/
+
+  private Float32(dv: DataView, pos: number): ParseFunctionReturn {
+    return { newPosition: pos + 4, data: dv.getFloat32(pos, true) };
+  }
+
+  private Float64(dv: DataView, pos: number): ParseFunctionReturn {
+    return { newPosition: pos + 8, data: dv.getFloat64(pos, true) };
+  }
+
+  private Uint8(dv: DataView, pos: number): ParseFunctionReturn {
+    return { newPosition: pos + 1, data: dv.getUint8(pos) };
+  }
+
+  private Uint16(dv: DataView, pos: number): ParseFunctionReturn {
+    return { newPosition: pos + 2, data: dv.getUint16(pos, true) };
+  }
+
+  private Uint32(dv: DataView, pos: number): ParseFunctionReturn {
+    return { newPosition: pos + 4, data: dv.getUint32(pos, true) };
+  }
+
+  private Uint64(dv: DataView, pos: number): ParseFunctionReturn {
+    return {
+      newPosition: pos + 4,
+      data: dv.getUint32(pos, true) + 2 ** 32 * dv.getUint32(pos + 8, true),
+    };
+  }
+
+  private CString(dv: DataView, pos: number): ParseFunctionReturn {
+    const u8 = new Uint8Array(dv.buffer, pos);
+    const end = u8.findIndex((v) => v === 0);
+
+    return {
+      newPosition: pos + end,
+      data: String.fromCharCode.apply(null, new Uint8Array(u8.slice(0, end))),
+    };
+  }
+
+  private FixedArray(dv: DataView, pos: number, type: DataType | string, length: number): ParseFunctionReturn {
+    return {
+      // TODO
+      newPosition: -1,
+      data: {},
+    };
+  }
+
+  private DynArray(dv: DataView, pos: number, type: DataType | string): ParseFunctionReturn {
+    let arrayLength = dv.getUint32(pos);
+    let arrayOffset = dv.getUint32(pos + 4);
+    if (arrayOffset === 0) {
+      // 0 is the place of the offset itself, it cannot be the content of the array itself.
+      throw new Error("DynArray offset cannot be 0");
+    }
+    let arrayPtr = pos + 4 + arrayOffset;
+
+    return this.FixedArray(dv, arrayPtr, type, arrayLength);
+  }
+
+  private RefArray(dv: DataView, pos: number, type: DataType | string): ParseFunctionReturn {
+    return {
+      // TODO
+      newPosition: -1,
+      data: {},
+    };
+  }
+
+  private Pointer(dv: DataView, pos: number, type: DataType | string): ParseFunctionReturn {
+    return {
+      // TODO
+      newPosition: -1,
+      data: {},
+    };
+  }
+
+  private String16(dv: DataView, pos: number): ParseFunctionReturn {
+    return {
+      // TODO
+      newPosition: -1,
+      data: {},
+    };
+  }
+
+  private Filename(dv: DataView, pos: number): ParseFunctionReturn {
+    return {
+      // TODO
+      newPosition: -1,
+      data: {},
+    };
+  }
+
+  private Fileref(dv: DataView, pos: number): ParseFunctionReturn {
+    return {
+      // TODO
+      newPosition: -1,
+      data: {},
+    };
+  }
+
+  private Unknown(dv: DataView, pos: number): ParseFunctionReturn {
+    throw new Error("Could not parse unknown data");
+  }
+
+  /**
+   *      Parser utils
+   **/
+
+  private _getOptimisedArrayConstructor(baseType): Function {
+    switch (baseType) {
+      case BaseType.Float32:
+        return Float32Array;
+
+      case BaseType.Float64:
+        return Float64Array;
+
+      case BaseType.Uint8:
+        return Uint8Array;
+
+      case BaseType.Uint16:
+        return Uint16Array;
+
+      case BaseType.Uint32:
+        return Uint32Array;
+    }
   }
 }
