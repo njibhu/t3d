3b5dbe0826fd63118c25eeaa6c90b44a2dc21d22
Njibhu <manu@njibhu.eu>
2020-09-15 23:11:55 +0200

    Add command to gulp to copy assets to examples and explorer dist folder

--- library/gulpfile.js
+++ library/gulpfile.js
@@ -1,40 +1,47 @@
-'use strict';
-
-//Load version from package.json
-const version = require('./package.json').version;
+"use strict";
 
 /* Gulp modules and requires */
-const browserify = require('browserify');
-const gulp = require('gulp');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const sourcemaps = require('gulp-sourcemaps');
-const log = require('gulplog');
-const uglifyjs = require('uglify-es');
-const composer = require('gulp-uglify/composer');
+const browserify = require("browserify");
+const gulp = require("gulp");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const sourcemaps = require("gulp-sourcemaps");
+const log = require("gulplog");
+const uglifyjs = require("uglify-es");
+const composer = require("gulp-uglify/composer");
 const uglify = composer(uglifyjs, console);
 
-gulp.task('T3D', function(){
-	// set up the browserify instance on a task basis
-	let b = browserify({
-		entries: './src/T3DLib.js',
-		debug: true,
-		standalone: 'T3D'
-	});
+gulp.task("T3D", function() {
+  // set up the browserify instance on a task basis
+  let b = browserify({
+    entries: "./src/T3DLib.js",
+    debug: true,
+    standalone: "T3D",
+  });
 
-	return b.bundle()
-		.pipe(source(`T3D.js`))
-		.pipe(buffer())
-		.pipe(sourcemaps.init({loadMaps: true}))
-			// Add transformation tasks to the pipeline here.
-			.pipe(uglify())
-			.on('error', log.error)
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('build'));
+  return (
+    b
+      .bundle()
+      .pipe(source(`T3D.js`))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      // Add transformation tasks to the pipeline here.
+      .pipe(uglify())
+      .on("error", log.error)
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest("build"))
+  );
 });
 
-gulp.task('watch', function() {
-	gulp.watch(['src/**/*.js'], gulp.series('T3D'));
+gulp.task("copy", function() {
+  return gulp
+    .src([`./build/T3D.js`, `./build/T3D.js.map`])
+	.pipe(gulp.dest("../examples/dist/static"))
+	.pipe(gulp.dest("../explorer/dist/static"));
 });
-  
-gulp.task('default', gulp.series('T3D'));
+
+gulp.task("watch", function() {
+  gulp.watch(["src/**/*.js"], gulp.series("T3D"));
+});
+
+gulp.task("default", gulp.series("T3D"));
