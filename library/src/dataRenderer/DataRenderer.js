5ef3e5c9023e1fed1aa30dd946c659e73650361c
Njibhu <manu@njibhu.eu>
2020-09-18 01:20:32 +0200

    DataRenderer

--- library/src/dataRenderer/DataRenderer.js
+++ library/src/dataRenderer/DataRenderer.js
@@ -54,128 +54,130 @@ const GW2File = require("../format/file/GW2File");
  * @param  {Object} context      Shared value object between renderers.
  * @param  {Logger} logger       The logging class to use for progress, warnings, errors et cetera.
  */
-let DataRenderer = function(localReader, settings, context, logger) {
-  /// Just storing parameters
-  this.localReader = localReader;
-  this.settings = settings;
-  if (!settings) {
-    settings = {};
+class DataRenderer {
+  constructor(localReader, settings, context, logger) {
+    /// Just storing parameters
+    this.localReader = localReader;
+    this.settings = settings;
+    if (!settings) {
+      settings = {};
+    }
+    this.context = context;
+    this.context[this.constructor.name] = {};
+
+    if (logger) this.logger = logger;
+    else this.logger = T3D.Logger;
   }
-  this.context = context;
-  this.context[this.constructor.name] = {};
 
-  if (logger) this.logger = logger;
-  else this.logger = T3D.Logger;
-};
+  /**
+   * Gets the output value object for a specified class within the context.
+   *
+   * @param  {Class} otherClass The class to fetch the output value object for.
+   * If not specified the class of this instance will be used.
+   * @return {Object}            The output value object for this class within the context.
+   */
+  getOutput(otherClass) {
+    return this.context[otherClass ? otherClass.name : this.constructor.name];
+  }
 
-/**
- * Gets the output value object for a specified class within the context.
- *
- * @param  {Class} otherClass The class to fetch the output value object for.
- * If not specified the class of this instance will be used.
- * @return {Object}            The output value object for this class within the context.
- */
-DataRenderer.prototype.getOutput = function(otherClass) {
-  return this.context[otherClass ? otherClass.name : this.constructor.name];
-};
+  /**
+   * Basic rendering of unknown files. Output fileds generated:
+   *
+   * - *fileId* The fileId passed in the settings parameter when constructing this instance.
+   *
+   * - *rawData* An ArrayBuffer containg the infalted binary data of the loaded file.
+   *
+   * - *rawString* A string representation of the rawData
+   *
+   * - *image* A value object witht he fields 'width', 'height' and 'data' describing a RGBA bitmap
+   * image. Only set if the loaded file was a texture.
+   *
+   * - *file* A GW2File representation of the loaded file. Only set if the loaded file was a Pack File.
+   *
+   * @async
+   * @param  {Function} callback Fires when renderer is finished, does not take arguments.
+   */
+  renderAsync(callback) {
+    let self = this;
 
-/**
- * Basic rendering of unknown files. Output fileds generated:
- *
- * - *fileId* The fileId passed in the settings parameter when constructing this instance.
- *
- * - *rawData* An ArrayBuffer containg the infalted binary data of the loaded file.
- *
- * - *rawString* A string representation of the rawData
- *
- * - *image* A value object witht he fields 'width', 'height' and 'data' describing a RGBA bitmap
- * image. Only set if the loaded file was a texture.
- *
- * - *file* A GW2File representation of the loaded file. Only set if the loaded file was a Pack File.
- *
- * @async
- * @param  {Function} callback Fires when renderer is finished, does not take arguments.
- */
-DataRenderer.prototype.renderAsync = function(callback) {
-  let self = this;
+    this.localReader.loadFile(this.settings.id, function(inflatedData) {
+      /// Set fileId so callers can identify this VO
+      self.getOutput().fileId = self.settings.id;
 
-  this.localReader.loadFile(this.settings.id, function(inflatedData) {
-    /// Set fileId so callers can identify this VO
-    self.getOutput().fileId = self.settings.id;
+      /// Share inflated data
+      self.getOutput().rawData = inflatedData;
 
-    /// Share inflated data
-    self.getOutput().rawData = inflatedData;
+      /// Construct raw string
+      let uarr = new Uint8Array(inflatedData);
+      let rawStrings = [];
+      let chunksize = 0xffff;
+      let len = Math.min(uarr.length, 10000);
 
-    /// Construct raw string
-    let uarr = new Uint8Array(inflatedData);
-    let rawStrings = [];
-    let chunksize = 0xffff;
-    let len = Math.min(uarr.length, 10000);
+      // There is a maximum stack size. We cannot call String.fromCharCode with as many arguments as we want
+      for (let i = 0; i * chunksize < len; i++) {
+        rawStrings.push(
+          String.fromCharCode.apply(
+            null,
+            uarr.subarray(i * chunksize, (i + 1) * chunksize)
+          )
+        );
+      }
 
-    // There is a maximum stack size. We cannot call String.fromCharCode with as many arguments as we want
-    for (let i = 0; i * chunksize < len; i++) {
-      rawStrings.push(
-        String.fromCharCode.apply(
-          null,
-          uarr.subarray(i * chunksize, (i + 1) * chunksize)
-        )
-      );
-    }
+      if (len < uarr.length) {
+        rawStrings.push(
+          "T3D Ignored the last " +
+            (uarr.length - len) +
+            " bytes when generating this raw output"
+        );
+      }
 
-    if (len < uarr.length) {
-      rawStrings.push(
-        "T3D Ignored the last " +
-          (uarr.length - len) +
-          " bytes when generating this raw output"
-      );
-    }
+      self.getOutput().rawString = rawStrings.join();
 
-    self.getOutput().rawString = rawStrings.join();
+      /// Check if this is an PF or ATEX file
+      // Binareis are MZ
+      let ds = new DataStream(inflatedData);
+      let first4 = ds.readCString(4);
 
-    /// Check if this is an PF or ATEX file
-    // Binareis are MZ
-    let ds = new DataStream(inflatedData);
-    let first4 = ds.readCString(4);
-
-    /// Do special stuff for different fcc signatures
-    ///
-    /// fourcc != fcc::ATEX && fourcc != fcc::ATEC && fourcc != fcc::ATEP &&
-    /// fourcc != fcc::ATET && fourcc != fcc::ATEU && fourcc != fcc::ATTX)
-    ///
-    if (
-      first4 === "ATEX" ||
-      first4 === "ATEC" ||
-      first4 === "ATEP" ||
-      first4 === "ATET" ||
-      first4 === "ATEU" ||
-      first4 === "ATTX"
-    ) {
-      /// TODO: MOVE TO GW2 texture file!!
-      /// Load file using LocalReader.
-      self.localReader.loadTextureFile(self.settings.id, function(
-        inflatedData,
-        dxtType,
-        imageWidth,
-        imageHeigth
+      /// Do special stuff for different fcc signatures
+      ///
+      /// fourcc != fcc::ATEX && fourcc != fcc::ATEC && fourcc != fcc::ATEP &&
+      /// fourcc != fcc::ATET && fourcc != fcc::ATEU && fourcc != fcc::ATTX)
+      ///
+      if (
+        first4 === "ATEX" ||
+        first4 === "ATEC" ||
+        first4 === "ATEP" ||
+        first4 === "ATET" ||
+        first4 === "ATEU" ||
+        first4 === "ATTX"
       ) {
-        /// Create image using returned data.
-        let image = {
-          data: new Uint8Array(inflatedData),
-          width: imageWidth,
-          height: imageHeigth
-        };
+        /// TODO: MOVE TO GW2 texture file!!
+        /// Load file using LocalReader.
+        self.localReader.loadTextureFile(self.settings.id, function(
+          inflatedData,
+          dxtType,
+          imageWidth,
+          imageHeigth
+        ) {
+          /// Create image using returned data.
+          let image = {
+            data: new Uint8Array(inflatedData),
+            width: imageWidth,
+            height: imageHeigth
+          };
 
-        self.getOutput().image = image;
+          self.getOutput().image = image;
+          callback();
+        });
+      } else if (first4.indexOf("PF") === 0) {
+        self.getOutput().file = new GW2File(ds, 0);
         callback();
-      });
-    } else if (first4.indexOf("PF") === 0) {
-      self.getOutput().file = new GW2File(ds, 0);
-      callback();
-    } else {
-      self.getOutput().file = null;
-      callback();
-    }
-  });
-};
+      } else {
+        self.getOutput().file = null;
+        callback();
+      }
+    });
+  }
+}
 
 module.exports = DataRenderer;
