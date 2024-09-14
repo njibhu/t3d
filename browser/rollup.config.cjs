ec81e844b05b938a46f2caa5be8d011967ec1051
Njibhu <manu@njibhu.eu>
2024-09-14 18:49:12 +0200

    Get rid of lerna and gulp

---
+++ browser/rollup.config.cjs
@@ -0,0 +1,32 @@
+const copy = require('rollup-plugin-copy');
+
+module.exports = {
+  input: './src/index.js',
+  output: {
+    file: './dist/index.js',
+    format: 'iife',
+    sourcemap: true
+  },
+  plugins: [
+    copy({
+      targets: [
+        { src: '../t3dtools.js/t3dworker.js', dest: './dist/static' },
+        { src: '../t3dtools.js/t3dworker.wasm', dest: './dist/static' },
+        { src: '../node_modules/w2ui/w2ui-1.4.3.min.js', dest: './dist/static' },
+        { src: '../node_modules/w2ui/w2ui-1.4.3.min.css', dest: './dist/static' },
+        { src: '../node_modules/three/build/three.js', dest: './dist/static' },
+        { src: '../node_modules/three/examples/js/controls/FlyControls.js', dest: './dist/static' },
+        { src: '../node_modules/three/examples/js/controls/PointerLockControls.js', dest: './dist/static' },
+        { src: '../node_modules/three/examples/js/controls/OrbitControls.js', dest: './dist/static' },
+        { src: '../node_modules/three/examples/js/exporters/OBJExporter.js', dest: './dist/static' },
+        { src: '../node_modules/jquery/dist/jquery.js', dest: './dist/static' },
+        { src: '../node_modules/DataStream.js/DataStream.js', dest: './dist/static' },
+        { src: '../library/build/T3D.js', dest: './dist/static' },
+        { src: '../library/build/T3D.js.map', dest: './dist/static' },
+        { src: '../parser/build/t3d-parser.js', dest: './dist/static' },
+        { src: '../parser/build/t3d-parser.js.map', dest: './dist/static' },
+        { src: './src/index.html', dest: './dist/' }
+      ]
+    })
+  ]
+};
---
+++ explorer/rollup.config.cjs
@@ -0,0 +1,43 @@
+const copy = require('rollup-plugin-copy');
+const commonjs = require('@rollup/plugin-commonjs');
+const resolve = require('@rollup/plugin-node-resolve');
+
+module.exports = {
+  input: "src/index.js",
+  output: {
+    dir: 'dist/',
+    format: 'es',
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    copy({
+      targets: [
+        { src: 'src/*.html', dest: 'dist/' },
+        { src: 'src/*.css', dest: 'dist/' },
+      ],
+    }),
+    // Static dependencies
+    copy({
+      targets: [
+        `../t3dtools.js/t3dworker.js`,
+        `../t3dtools.js/t3dworker.wasm`,
+        `../node_modules/three/build/three.js`,
+        `../node_modules/three/examples/js/controls/FlyControls.js`,
+        `../node_modules/three/examples/js/controls/OrbitControls.js`,
+        `../node_modules/three/examples/js/controls/PointerLockControls.js`,
+        `../node_modules/three/examples/js/libs/stats.min.js`,
+        `../node_modules/jquery/dist/jquery.js`,
+        `../node_modules/DataStream.js/DataStream.js`,
+        `../library/build/T3D.js`,
+        `../library/build/T3D.js.map`,
+        `../parser/build/t3d-parser.js`,
+        `../parser/build/t3d-parser.js.map`,
+        `./external-assets/gw2/Background1_2400_1200.jpg`,
+        `./external-assets/gw2/Background2_3840_2026.jpg`,
+        `./external-assets/gw2/Background3_4096x2027.jpg`,
+        `./external-assets/github/GitHub-Mark-120px-plus.png`,
+      ].map(src => ({ src, dest: 'dist/static' })),
+    }),
+  ],
+};
